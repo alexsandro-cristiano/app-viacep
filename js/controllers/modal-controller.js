1227ccb7beb61db7703a205dc55e162873898f52
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-06-02 07:21:55 -0300

    modifica comportamento no modal

--- js/controllers/modal-controller.js
+++ js/controllers/modal-controller.js
@@ -1,33 +1,32 @@
 function State() {
-  this.container = null;
-  this.btncloser = null;
+  this.container = null
+  this.btncloser = null
 }
 
 function handleBtnCloseClick(event) {
-  event.preventDefault();
-  closeModal();
+  event.preventDefault()
+  closeModal()
 }
 
 function handleCloseClick(event) {
-  event.preventDefault();
   if (event.target === this) {
-    closeModal();
+    closeModal()
   }
 }
-const state = new State();
+const state = new State()
 
 export function openModal() {
-  state.container.classList.add("active");
+  state.container.classList.add('active')
 }
 
 export function closeModal() {
-  state.container.classList.remove("active");
+  state.container.classList.remove('active')
 }
 
 export function init() {
-  state.container = document.querySelector("#modal-contact");
-  state.btncloser = document.querySelector("#close-contact");
+  state.container = document.querySelector('#modal-contact')
+  state.btncloser = document.querySelector('#close-contact')
 
-  state.btncloser.addEventListener("click", handleBtnCloseClick);
-  state.container.addEventListener("click", handleCloseClick);
+  state.btncloser.addEventListener('click', handleBtnCloseClick)
+  state.container.addEventListener('click', handleCloseClick)
 }
